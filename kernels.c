fc6872b4d7e409846a279ca9d03a5fead5271d49
Chris Wailes <chris.wailes@gmail.com>
2011-11-02 15:25:29 -0600

    First pass at the pivot kernel.

--- kernels.c
+++ kernels.c
@@ -13,5 +13,21 @@
 // Functions
 
 void pivot_kernel(dictionary* dict) {
+	int e_and_l[2];
 	
+	while (!dictionary_is_final(dict)) {
+		select_entering_and_leaving(dict, &e_and_l);
+		
+		/*
+		 * If the leaving variable is -1 we need to swap the bounds on the
+		 * selected entering variable.  Otherwise we need to pivot on the
+		 * entering and leaving variables.
+		 */
+		if (e_and_l[1] == -1) {
+			dict->var_rests[e_and_l[0]] = (dict->var_rests[e_and_l[0]] == UPPER ? LOWER : UPPER);
+			
+		} else {
+			dictionary_pivot(dict, e_and_l[0], e_and_l[1]);
+		}
+	}
 }
